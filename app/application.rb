09cdd78f19a1e24b30f6c6b396915b151dfb8509
Jonathan Wong <jonathan.wong110@gmail.com>
2019-06-23 20:03:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,6 +24,7 @@ class Application
   def handle_search(search_term)
     if @@items.include?(search_term)
       return "#{search_term} is one of our items"
+      @carts << @item
     else
       return "Couldn't find #{search_term}"
     end
